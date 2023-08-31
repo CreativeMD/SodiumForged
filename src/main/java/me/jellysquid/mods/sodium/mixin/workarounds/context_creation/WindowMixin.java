d46df6d2d957744d27e39a8ebd5884ffb1bf8756
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-08-31 23:11:05 +0200

    Finished port to official mappings

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/context_creation/WindowMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/context_creation/WindowMixin.java
@@ -3,10 +3,6 @@ package me.jellysquid.mods.sodium.mixin.workarounds.context_creation;
 import me.jellysquid.mods.sodium.client.util.workarounds.PostLaunchChecks;
 import me.jellysquid.mods.sodium.client.util.workarounds.Workarounds;
 import me.jellysquid.mods.sodium.client.util.workarounds.driver.nvidia.NvidiaWorkarounds;
-import net.minecraft.client.WindowEventHandler;
-import net.minecraft.client.WindowSettings;
-import net.minecraft.client.util.MonitorTracker;
-import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,11 +10,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.mojang.blaze3d.platform.DisplayData;
+import com.mojang.blaze3d.platform.ScreenManager;
+import com.mojang.blaze3d.platform.Window;
+import com.mojang.blaze3d.platform.WindowEventHandler;
 
 @Mixin(Window.class)
 public class WindowMixin {
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
     private long wrapGlfwCreateWindow(int width, int height, CharSequence title, long monitor, long share) {
+        This does not work on forge, invoke is missing
         if (Workarounds.isWorkaroundEnabled(Workarounds.Reference.NVIDIA_THREADED_OPTIMIZATIONS)) {
             NvidiaWorkarounds.install();
         }
@@ -33,7 +34,7 @@ public class WindowMixin {
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void postWindowCreated(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
+    private void postWindowCreated(WindowEventHandler eventHandler, ScreenManager monitorTracker, DisplayData settings, String videoMode, String title, CallbackInfo ci) {
         PostLaunchChecks.checkContext();
     }
 }
